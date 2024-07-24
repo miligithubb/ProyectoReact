cb225e581849732b3451866844e8313224a4bf3b
Milagros Agustina Rocha <milagrosagustinarocha@MacBook-Air-de-Milagros.local>
2024-07-23 23:33:17 -0300

    Comienzo de proyecto

---
+++ mi-primera-appp/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from './components/Button'
+import ItemListContainer from './components/ItemListContainer'
+import NavBar from './components/NavBar'
+
+export function App() {
+
+    
+  return (
+    <>
+    <NavBar/>
+    <ItemListContainer greeting="HOLA"/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ mi-primera-appp/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h2>TIENDA</h2>
+        <CartWidget/>
+    </nav>
+  )
+}
+
+export default NavBar
